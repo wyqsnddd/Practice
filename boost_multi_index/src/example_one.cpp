f28764227cd74378467676fea77dba9367c71734
yuquan <wyqsnddd@gmail.com>
2017-08-13 01:05:57 +0200

    (1) add composite key first feasibility and then cost (2) count 'bool' on hashed_non_unique

--- boost_multi_index/src/example_one.cpp
+++ boost_multi_index/src/example_one.cpp
@@ -1,10 +1,9 @@
 # include <boost/multi_index_container.hpp>
-
 # include <boost/multi_index/hashed_index.hpp>
 # include <boost/multi_index/ordered_index.hpp>
 // Sequenced_index allows you to treat a MultiIndex container like a list of type std::list. Elements are stored in the given order.
 # include <boost/multi_index/sequenced_index.hpp>
-
+#include <boost/multi_index/composite_key.hpp>
 
 # include <boost/multi_index/identity.hpp>
 # include <boost/multi_index/member.hpp>
@@ -20,14 +19,16 @@
 
 struct mockPath
 {
-  mockPath(int id, int cost, int group_number, const std::string& name){ 
+  mockPath(int id, int cost, int group_number, bool feasibility, const std::string& name){ 
 
     id_ = id;
     cost_ = cost;
 
     group_number_ = group_number;
     name_ = name;
-      }
+    feasibility_ = feasibility;
+  }
+
   // sort by cost
   bool operator<(const mockPath& e)const{return cost_<e.cost_;}
   int id_;
@@ -35,6 +36,8 @@ struct mockPath
   int group_number_;
   std::string name_;
 
+
+  bool feasibility_;
   
   friend std::ostream& operator<<(std::ostream& os,const mockPath& e)
   {
@@ -43,7 +46,9 @@ struct mockPath
   }
   friend std::ostream& operator<<(std::ostream& os,const mockPath* e)
   {
-    os<<"ID: "<<e->id_<<" group number: "<< e->group_number_<<" name: "<<e->name_<<" cost: "<<e->cost_<<std::endl;
+    os<<
+      "Feasibility: "<<e->feasibility_<< 
+      " ID: "<<e->id_<<" group number: "<< e->group_number_<<" name: "<<e->name_<<" cost: "<<e->cost_<<std::endl;
     return os;
   }
 
@@ -69,6 +74,7 @@ private:
 struct tag_name {};
 struct tag_id {};
 struct tag_as_inserted {};
+struct tag_composite {};
 
 typedef boost::multi_index::multi_index_container<
   boost::shared_ptr<mockPath>,
@@ -90,10 +96,22 @@ typedef boost::multi_index::multi_index_container<
       boost::multi_index::tag<tag_as_inserted>
       >,
     // (4) access to an element with position
-    boost::multi_index::random_access<>
-    >
+    boost::multi_index::random_access<>,
+    // (5) count on the bool: feasibility so we use hash_non_unique instead of hash_unique
+    // If we count on words, we use orderd_non_unique<>.
+    boost::multi_index::hashed_non_unique<
+      boost::multi_index::member<mockPath, bool, &mockPath::feasibility_>
+      >,
+    // (6) We use composite keys to sort two keys in a lexicographical order: (1) feasibility (2) cost
+    boost::multi_index::ordered_non_unique<
+      boost::multi_index::tag<tag_composite>, boost::multi_index::composite_key<
+	mockPath,
+	boost::multi_index::member<mockPath, bool, &mockPath::feasibility_>,
+	boost::multi_index::identity<mockPath>      
+	>
+      >
+    >// end of indexed_by
   > path_set;
-
 
 void print_out_by_name(const path_set & inputSet){
   // get a view to index 1 name
@@ -113,9 +131,24 @@ void print_out_by_cost(const path_set & inputSet){
 
 }
 
+void print_out_composite_key(const path_set & inputSet){
+  // get a view to index 1 name
+  const path_set::index<tag_composite>::type & composite_index = inputSet.get<tag_composite>();
+  std::copy(
+	    composite_index.begin(),composite_index.end(),
+	    std::ostream_iterator<boost::shared_ptr<mockPath>>(std::cout));
+
+}
+
 void delete_object(int id, path_set & input_container){
   // Note that the erase operation needs to work on an index not a container. 
   path_set::index<tag_id>::type & test_id_index = input_container.get<tag_id>();
+
+  int amount = test_id_index.count(id);
+  if (amount){
+    std::cout<<"The amount of  id: "<<id<< " is:  "<<amount <<std::endl;  
+    return;
+  }
   path_set::index<tag_id>::type::iterator it = test_id_index.find(id);
   std::cout << "Found host id " << (*it)->id_ <<
     ". Attempting to delete. input_container.size() before is " << input_container.size() << " and ";
@@ -131,19 +164,19 @@ int main(){
   // (0) Basic insertion 
   path_set sample_container;
   boost::shared_ptr<mockPath>  newptr;
-  newptr.reset(new mockPath(21, 1, 2, "hello_world"));
+  newptr.reset(new mockPath(21, 1, 2, true, "hello_world"));
   sample_container.insert(newptr);
-  newptr.reset(new mockPath(20, 1, 2, "hello_world"));
+  newptr.reset(new mockPath(20, 1, 2, false, "hello_world"));
   sample_container.insert(newptr);
-  newptr.reset(new mockPath(2123, 2, 3, "bei_world"));
+  newptr.reset(new mockPath(2123, 2, 3, true, "bei_world"));
   sample_container.insert(newptr);
-  newptr.reset(new mockPath(234, 2, 4, "bei_world") );
+  newptr.reset(new mockPath(234, 2, 4, false, "bei_world") );
   sample_container.insert(newptr);
-  newptr.reset(new mockPath(1243, 1, 2, "bei_Mu") );
+  newptr.reset(new mockPath(1243, 1, 2, true,  "bei_Mu") );
   sample_container.insert(newptr);
-  newptr.reset(new mockPath(8987, 1, 2, "how_are_you") );
+  newptr.reset(new mockPath(8987, 1, 2, false, "how_are_you") );
   sample_container.insert(newptr);
-  newptr.reset(new mockPath(1117, 1, 2, "how_are_you") );
+  newptr.reset(new mockPath(1117, 1, 2, true,  "how_are_you") );
   sample_container.insert(newptr);
  
 
@@ -154,6 +187,13 @@ int main(){
   std::cout<<"Print out by cost: "<<std::endl;
   print_out_by_cost(sample_container);
 
+  const path_set::nth_index<0>::type & test_cost_index = sample_container.get<0>();
+
+  /// Note that this 'count' operation has logarithmic complexity
+  std::cout<<"Number of path with a cost equals to 2 is: "<<test_cost_index.count( mockPath(1117, 1, 2, false, "how_are_you") ) <<std::endl;
+  
+  
+  
 
   // (2) ordered by name
   const path_set::index<tag_name>::type & name_index = sample_container.get<tag_name>();
@@ -162,6 +202,9 @@ int main(){
   else
     std::cout<<"Found name: test "<<std::endl;
 
+  std::cout<<"Number of path with a name 'hello_world' is: "<<name_index.count( "hello_world" ) <<std::endl;
+
+  
   // (3) ordererd by id
   // not a constant index as we want to delete and change object   
   path_set::index<tag_id>::type & id_index = sample_container.get<tag_id>();
@@ -191,7 +234,15 @@ int main(){
   std::cout<<"The amount of  id '881' is:  "<<id_index.count(881) <<std::endl;
 
   // (5.2) deletion:
-  delete_object(234, sample_container);
+  newptr.reset(new mockPath(64, 2, 4, false, "bei_world") );
+  std::cout<<"Before insertion the container size is: "<<sample_container.size()<<std::endl;
+  sample_container.insert(newptr);
+  std::cout<<"Inserted path with id: 64"<<std::endl;
+  std::cout<<"After insertion the container size is: "<<sample_container.size()<<std::endl;
+  print_out_by_name(sample_container);
+
+  
+  delete_object(64, sample_container);
   
   // (6) sequence: works as a std::list. easy to get front and end. 
   const path_set::index<tag_as_inserted>::type & inserted_index = sample_container.get<tag_as_inserted>();
@@ -217,6 +268,15 @@ int main(){
     std::cout << rand_index[i]->name_ << '\n';
 
   std::cout<<"The container size is: "<<sample_container.size()<<std::endl;
+  
+  // (8) Count on the feasible path
+  const path_set::nth_index<5>::type& feasibility_index=sample_container.get<5>();
+  std::cout<<"The number of feasible path is: "<<feasibility_index.count(true)<<std::endl;
+  std::cout<<"The number of infeasible path is: "<<feasibility_index.count(false)<<std::endl;
 
+  print_out_by_name(sample_container);
+  // (9) print out by the composite key
+  std::cout<<std::endl<<" Container sorted by the composite key (1) feasibility (2) cost"<<std::endl;
+  print_out_composite_key(sample_container);  
   
 }
